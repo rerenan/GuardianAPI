d61932e757d7881106d919d14199dddeff95b442
Renan Araujo <renanaraujo852@hotmail.com>
2023-05-19 16:34:48 -0300

    feat: create gradle project

---
+++ src/main/java/com/rerenan/guardianAPI/GuardianApiApplication.java
@@ -0,0 +1,13 @@
+package com.rerenan.guardianAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardianApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardianApiApplication.class, args);
+	}
+
+}
