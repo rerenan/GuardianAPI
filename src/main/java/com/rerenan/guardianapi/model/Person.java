0e3062b163d2d79bd3c0d0e57dbf49a7a8a51c8a
Renan Araujo <renanaraujo852@hotmail.com>
2023-05-22 05:28:42 -0300

    fix: config to deploy

--- src/main/java/com/rerenan/guardianapi/model/Person.java
+++ src/main/java/com/rerenan/guardianapi/model/Person.java
@@ -1,16 +1,12 @@
 package com.rerenan.guardianapi.model;
 
-import org.checkerframework.common.value.qual.MinLen;
-
-import com.rerenan.guardianapi.dto.UserDTO;
+import com.rerenan.guardianapi.dto.PersonDTO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -18,9 +14,11 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 
-public class User {
-    public User(UserDTO data) {
+public class Person {
+    public Person(PersonDTO data) {
         this.username = data.username();
+        this.email = data.email();
+        this.password = data.password();
 
     }
 
@@ -31,11 +29,9 @@ public class User {
     @Column(length = 35, nullable = false)
     private String username;
 
-    @Email
     @Column(length = 100, nullable = false)
     private String email;
 
-    @Size(min = 5, max = 100)
     @Column(length = 100, nullable = false)
     private String password;
 
