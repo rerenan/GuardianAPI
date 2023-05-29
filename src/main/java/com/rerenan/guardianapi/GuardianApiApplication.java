b734b8ec6fba4d34825ed415e76d3d2f52c6a169
Renan Araujo <renanaraujo852@hotmail.com>
2023-05-29 08:40:10 -0300

    config: environments and encrypt framework

--- src/main/java/com/rerenan/guardianapi/GuardianApiApplication.java
+++ src/main/java/com/rerenan/guardianapi/GuardianApiApplication.java
@@ -2,9 +2,13 @@ package com.rerenan.guardianapi;
 
 import org.springframework.boot.SpringApplication;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@PropertySource(name = "EncryptedProperties", value = "classpath:encrypted.properties")
 
 public class GuardianApiApplication {
 
